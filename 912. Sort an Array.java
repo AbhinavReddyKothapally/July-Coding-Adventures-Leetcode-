ec03e4830da5cb7e36c57e01fc06a73a33bb362a
Abhinav Reddy Kothapally <149865334+AbhinavReddyKothapally@users.noreply.github.com>
2024-07-25 11:03:16 +0530

    Sort an Array

---
+++ 912. Sort an Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        pq.add(nums[i]);
+        for(int i=0;i<n;i++)
+        nums[i]=pq.poll();
+        return nums;
+    }
+}
